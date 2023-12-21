86a948ce2c65251ca73318b635b7683ce9992bbf
Elliot Walmsley <elliotwalmsley@gmail.com>
2023-12-21 12:19:57 +0000

    Remove indent on markdown for easy copy/paste Reduce size of cli prompt

--- gptcli/cli.py
+++ gptcli/cli.py
@@ -9,6 +9,7 @@ from prompt_toolkit.key_binding.bindings import named_commands
 from rich.console import Console
 from rich.live import Live
 from rich.markdown import Markdown
+from .markdown import CustomMarkdown
 from rich.text import Text
 
 from gptcli.session import (ALL_COMMANDS, COMMAND_CLEAR, COMMAND_QUIT,
@@ -16,11 +17,7 @@ from gptcli.session import (ALL_COMMANDS, COMMAND_CLEAR, COMMAND_QUIT,
                             ResponseStreamer, UserInputProvider)
 
 TERMINAL_WELCOME = """
-Hi! I'm here to help. Type `:q` or Ctrl-D to exit, `:c` or Ctrl-C and Enter to clear
-the conversation, `:r` or Ctrl-R to re-generate the last response.
-To enter multi-line mode, enter a backslash `\\` followed by a new line.
-Exit the multi-line mode by pressing ESC and then Enter (Meta+Enter).
-Try `:?` for help.
+Assistant:
 """
 
 
@@ -43,7 +40,7 @@ class StreamingMarkdownPrinter:
         self.current_text += text
         if self.markdown:
             assert self.live
-            content = Markdown(self.current_text, style="green")
+            content = CustomMarkdown(self.current_text, style="green")
             self.live.update(content)
             self.live.refresh()
         else:
@@ -84,7 +81,7 @@ class CLIChatListener(ChatListener):
 
     def on_chat_start(self):
         console = Console(width=80)
-        console.print(Markdown(TERMINAL_WELCOME))
+        console.print(CustomMarkdown(TERMINAL_WELCOME))
 
     def on_chat_clear(self):
         self.console.print("[bold]Cleared the conversation.[/bold]")
